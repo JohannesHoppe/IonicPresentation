f64a2046faa2f6af9af71a0872ff061d291623b0
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-11-16 20:24:49 +0100

    demo: fixing stuff here and there so that it looks nice

--- Demos/MdcTalks/js/app.js
+++ Demos/MdcTalks/js/app.js
@@ -1,19 +1,38 @@
 angular.module('app', ['ionic'])
-    .config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
             .state('side-menu1', {
                 url: '/menu',
                 templateUrl: 'side-menu1.html',
-                controller: function($scope, getData) {
+                controller: function ($scope, $ionicSideMenuDelegate, $timeout, talksService) {
 
-                    getData().then(function(data) {
-                        $scope.talks = data;
+                    $scope.talks = [
+                        {
+                            "title": "Lade Daten",
+                            "description": "Bitte warten Sie einen Moment, bis die Datan geladen sind.",
+                            "start": null,
+                            "speaker": null
+                        }
+                    ];
+
+                    talksService.loadTalkData().then(function(talks) {
+
+                        $scope.talks = talks;
+                        $scope.activeTalk = $scope.talks[talksService.getLastActiveIndex()];
                     });
 
+                    $scope.selectTalk = function (talk, index) {
+                        $scope.activeTalk = talk;
+                        talksService.setLastActiveIndex(index);
+                        $ionicSideMenuDelegate.toggleLeft(false);
+                    };
+
+                    $timeout(function () {
+                        $ionicSideMenuDelegate.toggleLeft(true);
+                    });
                 }
             });
 
         $urlRouterProvider.otherwise('/menu');
-
     });
